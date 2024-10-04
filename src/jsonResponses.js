9531fc77041c051b07409173beb4f5402b4e7b1b
Colin Zopf <caz1158@rit.edu>
2024-10-04 14:33:01 -0400

    basic functionality for get requests added to jsonResponses

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,3 +1,103 @@
 const fs = require('fs'); // pull in the file system module
 
 const dataset = fs.readFileSync(`${__dirname}/../assets/jsonDatasets/pokedex.json`);
+
+const getAll = (request, response, status) => {
+  const content = JSON.stringify(dataset);
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  });
+
+  // HEAD requests don't get a body with their response.
+  // Similarly, 204 status codes are "no content" responses
+  // so they also do not get a response body.
+  if (request.method !== 'HEAD' && status !== 204) {
+    response.write(content);
+  }
+
+  response.end();
+};
+
+const getByName = (request, response, status, name) => {
+  let content = JSON.stringify(dataset);
+  const contentArray = content.split(' ');
+  const arrayLength = contentArray.length;
+  content = '';
+  for (let i = 0; i < arrayLength; i++) {
+    if (contentArray[i].includes(name)) {
+      content += contentArray[i];
+    }
+  }
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  });
+
+  // HEAD requests don't get a body with their response.
+  // Similarly, 204 status codes are "no content" responses
+  // so they also do not get a response body.
+  if (request.method !== 'HEAD' && status !== 204) {
+    response.write(content);
+  }
+
+  response.end();
+};
+
+const getByType = (request, response, status, type) => {
+  let content = JSON.stringify(dataset);
+  const contentArray = content.split(' ');
+  const arrayLength = contentArray.length;
+  content = '';
+  for (let i = 0; i < arrayLength; i++) {
+    if (contentArray[i].includes(type)) {
+      content += contentArray[i];
+    }
+  }
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  });
+
+  // HEAD requests don't get a body with their response.
+  // Similarly, 204 status codes are "no content" responses
+  // so they also do not get a response body.
+  if (request.method !== 'HEAD' && status !== 204) {
+    response.write(content);
+  }
+
+  response.end();
+};
+
+const getByWeakness = (request, response, status, weakness) => {
+  let content = JSON.stringify(dataset);
+  const contentArray = content.split(' ');
+  const arrayLength = contentArray.length;
+  content = '';
+  for (let i = 0; i < arrayLength; i++) {
+    if (contentArray[i].includes(weakness)) {
+      content += contentArray[i];
+    }
+  }
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  });
+
+  // HEAD requests don't get a body with their response.
+  // Similarly, 204 status codes are "no content" responses
+  // so they also do not get a response body.
+  if (request.method !== 'HEAD' && status !== 204) {
+    response.write(content);
+  }
+
+  response.end();
+};
+
+// set out public exports
+module.exports = {
+  getAll,
+  getByType,
+  getByName,
+  getByWeakness,
+};
--- src/server.js
+++ src/server.js
@@ -8,44 +8,26 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const urlStruct = {
   '/': htmlHandler.getIndex,
   '/style.css': htmlHandler.getCSS,
-  '/getUsers': jsonHandler.getUsers,
-  '/notReal': jsonHandler.notFound,
+  '/getAll': jsonHandler.getAll,
+  '/getByName': jsonHandler.getByName,
+  '/getByType': jsonHandler.getByType,
+  '/getByWeakness': jsonHandler.getByWeakness,
   notFound: jsonHandler.notFound,
 };
 
 const parseBody = (request, response, handler) => {
-  // The request will come in in pieces, which we store in this array.
   const body = [];
-
-  // The body reassembly process is event driven, much like when we are streaming
-  // media like videos, etc. We will set up a few event handlers. This first one
-  // is for if there is an error. If there is, write it to the console and send
-  // back a 400-Bad Request error to the client.
   request.on('error', (err) => {
     console.dir(err);
     response.statusCode = 400;
     response.end();
   });
-
-  // The second possible event is the "data" event. This gets fired when we
-  // get a piece (or "chunk") of the body. Each time we do, we will put it in
-  // the array. We will always recieve these chunks in the correct order.
   request.on('data', (chunk) => {
     body.push(chunk);
   });
-
-  // The final event is when the request is finished sending and we have recieved
-  // all of the information. When the request "ends", we can proceed. Turn the body
-  // array into a single entity using Buffer.concat, then turn that into a string.
-  // With that string, we can use the querystring library to turn it into an object
-  // stored in bodyParams. We can do this because we know that the client sends
-  // us data in X-WWW-FORM-URLENCODED format. If it was in JSON we could use JSON.parse.
   request.on('end', () => {
     const bodyString = Buffer.concat(body).toString();
     request.body = query.parse(bodyString);
-
-    // Once we have the bodyParams object, we will call the handler function. We then
-    // proceed much like we would with a GET request.
     handler(request, response);
   });
 };
@@ -70,7 +52,8 @@ const onRequest = (request, response) => {
   // check if method was POST, otherwise check URLstruct for path.
   if (request.method === 'POST') {
     return handlePost(request, response, parsedUrl);
-  } if (urlStruct[parsedUrl.pathname]) {
+  }
+  if (urlStruct[parsedUrl.pathname]) {
     return urlStruct[parsedUrl.pathname](request, response);
   }
   return urlStruct.notFound(request, response);
