78e46752df1b924e76a07c156b820ac4877be7c6
Colin Zopf <caz1158@rit.edu>
2024-10-04 14:50:54 -0400

    functionality of jsonResponses expanded upon

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,17 +1,14 @@
 const fs = require('fs'); // pull in the file system module
 
-const dataset = fs.readFileSync(`${__dirname}/../assets/jsonDatasets/pokedex.json`);
+const dataset = JSON.parse(fs.readFileSync(`${__dirname}/../assets/jsonDatasets/pokedex.json`));
 
-const getAll = (request, response, status) => {
-  const content = JSON.stringify(dataset);
+const respondJSON = (request, response, status, object) => {
+  const content = JSON.stringify(object);
   response.writeHead(status, {
     'Content-Type': 'application/json',
     'Content-Length': Buffer.byteLength(content, 'utf8'),
   });
 
-  // HEAD requests don't get a body with their response.
-  // Similarly, 204 status codes are "no content" responses
-  // so they also do not get a response body.
   if (request.method !== 'HEAD' && status !== 204) {
     response.write(content);
   }
@@ -19,14 +16,16 @@ const getAll = (request, response, status) => {
   response.end();
 };
 
+const getAll = (request, response) => {
+  respondJSON(request, response, 200, dataset);
+};
+
 const getByName = (request, response, status, name) => {
-  let content = JSON.stringify(dataset);
-  const contentArray = content.split(' ');
-  const arrayLength = contentArray.length;
-  content = '';
+  const arrayLength = dataset.length;
+  let content = '';
   for (let i = 0; i < arrayLength; i++) {
-    if (contentArray[i].includes(name)) {
-      content += contentArray[i];
+    if (dataset[i].name.toUpperCase() === name.toUpperCase()) {
+      content += JSON.stringify(dataset[i]);
     }
   }
   response.writeHead(status, {
@@ -45,13 +44,11 @@ const getByName = (request, response, status, name) => {
 };
 
 const getByType = (request, response, status, type) => {
-  let content = JSON.stringify(dataset);
-  const contentArray = content.split(' ');
-  const arrayLength = contentArray.length;
-  content = '';
+  const arrayLength = dataset.length;
+  let content = '';
   for (let i = 0; i < arrayLength; i++) {
-    if (contentArray[i].includes(type)) {
-      content += contentArray[i];
+    if (dataset[i].name.toUpperCase() === type.toUpperCase()) {
+      content += JSON.stringify(dataset[i]);
     }
   }
   response.writeHead(status, {
@@ -70,13 +67,11 @@ const getByType = (request, response, status, type) => {
 };
 
 const getByWeakness = (request, response, status, weakness) => {
-  let content = JSON.stringify(dataset);
-  const contentArray = content.split(' ');
-  const arrayLength = contentArray.length;
-  content = '';
+  const arrayLength = dataset.length;
+  let content = '';
   for (let i = 0; i < arrayLength; i++) {
-    if (contentArray[i].includes(weakness)) {
-      content += contentArray[i];
+    if (dataset[i].name.toUpperCase() === weakness.toUpperCase()) {
+      content += JSON.stringify(dataset[i]);
     }
   }
   response.writeHead(status, {
@@ -94,10 +89,23 @@ const getByWeakness = (request, response, status, weakness) => {
   response.end();
 };
 
+// function for 404 not found requests with message
+const notFound = (request, response) => {
+  // create error message for response
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return a 404 with an error message
+  respondJSON(request, response, 404, responseJSON);
+};
+
 // set out public exports
 module.exports = {
   getAll,
   getByType,
   getByName,
   getByWeakness,
+  notFound,
 };
